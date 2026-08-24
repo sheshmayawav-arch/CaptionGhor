6afb0859c358acca966187bcbfc01189b540ae75
sheshmayawav-arch <sheshmayawav@gmail.com>
2026-08-24 20:45:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = "captionghor-v1";
+const CACHE_NAME = "captionghor-v2";
 
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/style.css",
     "/script.js",
-    "/manifest.json"
+    "/manifest.json",
+    "/icon-192.png",
+    "/icon-512.png"
 ];
 
 self.addEventListener("install", function (event) {
